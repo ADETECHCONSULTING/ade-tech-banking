94acfd9b58c80d7cf88d2495dd6cc466b152e70c
Adama <a.traore.pro@gmail.com>
2023-09-16 23:31:07 +0200

    feat: init project

---
+++ src/main/java/fr/adamatraore/banking/adetechbanking/AdeTechBankingApplication.java
@@ -0,0 +1,13 @@
+package fr.adamatraore.banking.adetechbanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdeTechBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdeTechBankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/adamatraore/banking/adetechbanking/AdeTechBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.adamatraore.banking.adetechbanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdeTechBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
